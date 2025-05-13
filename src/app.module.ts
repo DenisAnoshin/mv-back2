446bff1430581b08fce7bc774c63488932bd36d5
denisanoshin <mr.denis.anoshin@gmail.com>
2025-05-13 22:28:56 +0300

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,11 +20,11 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT, 10) || 5432,
-      username: process.env.DB_USERNAME || 'postgres',
-      password: process.env.DB_PASSWORD || 'postgres',
-      database: process.env.DB_NAME || 'chat',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [User, Group, UsersGroups, Message],
       synchronize: true, // ⚠️ не включай в продакшене!
     }),
